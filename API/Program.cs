fca1be8e7f8e5e7c328a2e67914b134892ba49c6
DanijelaMilanovic <danijelamilanovic222@gmail.com>
2022-10-02 15:57:10 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -14,12 +14,19 @@ internal class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+    
 
         builder.Services.AddDbContext<Persistence.DataContext>(options =>
         {
                          //the change occurs here.
                          //builder.cofiguration and not just configuration
             options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+
+           builder.Services.AddCors(opt => {
+            opt.AddPolicy("CorsPolicy",policy => {
+                policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+            });
         });
 
         var app = builder.Build();
@@ -47,6 +54,8 @@ internal class Program
         }
 
         app.UseHttpsRedirection();
+
+        app.UseCors("CorsPolicy");
 
         app.UseAuthorization();
 
